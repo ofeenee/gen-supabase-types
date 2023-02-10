76f33e107d53a8bbf523a52e31635001dcadf114
ofeenee <ofeenee@gmail.com>
2023-02-10 21:11:55 +0300

    basic functionality working

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// console.log(process.env);
+
+const [first, second, third, forth, fifth] = process.argv;
+
+console.log({ first, second, third, forth, fifth });
+// console.log(`Database URL variable name: ${DatabaseURLVariableName}`);
+
+// const DatabaseURL = process.env[DatabaseURLVariableName || 'SUPABASE_DATABASE_URL'];
+// if (!DatabaseURL) {
+//   console.error(`Database URL not found in environment variable ${DatabaseURLVariableName}`);
+//   process.exit(1);
+// }
+
+// execSync(`supabase gen types typescript --db-url ${DatabaseURL} > ./supabase.types.ts --debug`, {
+//   stdio: 'inherit',
+// });
