bf391972ea087410ec7434eb27850f6f4534c839
ofeenee <ofeenee@gmail.com>
2023-02-25 03:18:16 +0300

    add --debug --save-path --env-var --db-url flags to cli

--- index.js
+++ index.js
@@ -6,18 +6,56 @@ dotenv.config();
 
 // console.log(process.env);
 
-const [first, second, third, forth, fifth] = process.argv;
+const [first, second, ...rest] = process.argv;
+
+// console.log(rest);
+
 // console.log({ first, second, third, forth, fifth });
 
-const DatabaseURLVariableName = third;
-const DatabaseURL = third === '--db-url' ? forth : process.env[DatabaseURLVariableName || 'SUPABASE_DATABASE_URL'];
+// const DatabaseURLVariableName = third;
+
+const defaultConfig = {
+  '--env-var': 'SUPABASE_DATABASE_URL',
+  '--db-url': '',
+  '--save-path': './supabase.types.ts',
+  '--debug': true,
+};
+
+const overwriteConfig = getConfigFromEnv(rest);
+
+
+const config = { ...defaultConfig, ...overwriteConfig };
+
+// console.log({ default: defaultConfig, overwrite: overwriteConfig, current: config });
+
+const DatabaseURL = process.env[config['--db-url'] || config['--env-var']];
+const debug = config['--debug'];
+
 // console.log(`Database URL variable name: ${DatabaseURL}`);
 
 if (!DatabaseURL) {
-  console.error(`Database URL not found in environment variable ${DatabaseURLVariableName}`);
+  console.error(`Database URL not found: ${DatabaseURLVariableName}`);
   process.exit(1);
 }
 
-execSync(`supabase gen types typescript --db-url ${DatabaseURL} > ./supabase.types.ts --debug`, {
+execSync(`supabase gen types typescript --db-url ${DatabaseURL} > ${config['--save-path']} ${debug ? '--debug' : ''}`, {
   stdio: 'inherit',
 });
+
+
+function getConfigFromEnv(cmdFlags = []) {
+  const config = {};
+  if (!cmdFlags.length) return;
+
+  for (const flag of Object.keys(defaultConfig)) {
+    if (cmdFlags.includes(flag)) {
+      const index = cmdFlags.indexOf(flag);
+      config[flag] = cmdFlags[index + 1];
+      if (config[flag] === 'true' || config[flag] === 'false') {
+        config[flag] = config[flag] === 'true';
+      }
+    }
+  }
+
+  return config;
+}
